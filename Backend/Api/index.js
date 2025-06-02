311968be5783a1cb5ff0207540bcff935b172b22
itzsebas121 <sebastipan121@gmail.com>
2025-06-02 10:11:31 -0500

    refactor: rearrange app initialization for improved code clarity

--- Backend/Api/index.js
+++ Backend/Api/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
 
 const productosRouter = require("./routes/Product");
 const userRouter = require("./routes/User");
-const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(cors({
   origin: "*",
